04c93f7e0132318e5bb0242812f769cbff1f277e
fidhaashik <fidhacodenex@gmail.com>
2024-11-12 10:50:16 +0530

    update-logo

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,7 +1,7 @@
 <div class="flex items-start">
 <a class="flex items-center" href="{{ url('/') }}">
-    <img src="{{ asset('images/logo/logo.svg') }}" class="black_logo" alt="logo"> 
+    <img src="{{ asset('images/logo/Hala-Wa-Ghala-Logo.svg') }}" class="black_logo" alt="logo"> 
     {{-- <img src="{{ asset('images/dark-logo.svg') }}" class="white_logo" alt="logo"> --}}
-    <span class="text-l font-Inter font-bold text-slate-900 dark:text-white hidden xl:inline-block">Hala wa Ghala</span>
+    <span class="text-l font-Inter font-bold text-slate-900 dark:text-white hidden xl:inline-block">Hala Wa Ghala</span>
 </a>
 </div>
